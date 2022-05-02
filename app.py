188d6c82fe8cd923498634eae40e3818e27da336
AnaLavr <98440206+AnaLavr@users.noreply.github.com>
2022-05-02 14:33:03 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import database
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def northwind():
     facts=database.supplier()
     return render_template ('index.html', facts=facts )
+ 
+@app.route('/products/<int:supplier_id>')
+def products(supplier_id):
+    products =database.products(supplier_id)
+    header=database.supplier_name(supplier_id)
+    return render_template ('products.html', products=products,header=header )
+ 
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlite3
 def supplier(): 
     conn=sqlite3.connect('Northwind_large.sqlite') 
     cur=conn.cursor()    
-    cur.execute ('SELECT CompanyName, City, Country FROM  Supplier')  
+    cur.execute ('SELECT CompanyName, City, Country, Id FROM  Supplier')  
     
     column_names = []
     for column in cur.description:
@@ -19,3 +19,34 @@ def supplier():
         
     conn.close() 
     return dicts 
+
+def products(supplier_id):
+    conn=sqlite3.connect('Northwind_large.sqlite') 
+    cur=conn.cursor()    
+    cur.execute ('SELECT ProductName, QuantityPerUnit, UnitPrice From Product Where SupplierId=?',(supplier_id,) )  
+    column_names = []
+    for column in cur.description:
+        column_names.append(column[0])  
+    rows=cur.fetchall() 
+    dicts=[]
+    for row in rows:
+        d=dict(zip(column_names, row))
+        dicts.append(d)   
+    conn.close() 
+    return dicts 
+    
+    
+def supplier_name(supplier_id):
+    conn=sqlite3.connect('Northwind_large.sqlite') 
+    cur=conn.cursor()    
+    cur.execute ('SELECT CompanyName FROM Supplier WHERE Id=?',(supplier_id,) )  
+    column_names = []
+    for column in cur.description:
+        column_names.append(column[0])  
+    rows=cur.fetchall() 
+    dicts=[]
+    for row in rows:
+        d=dict( zip(column_names, row))
+        dicts.append(d)   
+    conn.close() 
+    return str(*row)
