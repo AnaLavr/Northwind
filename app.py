aa6280979cebae4d124872f6e603cdd8afbf15af
AnaLavr <98440206+AnaLavr@users.noreply.github.com>
2022-05-06 00:19:23 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -14,3 +14,14 @@ def products(supplier_id):
     header=database.supplier_name(supplier_id)
     return render_template ('products.html', products=products,header=header )
  
+ 
+@app.route('/categories') 
+def categories():
+    category=database.category()            
+    return render_template ('categories.html', category=category )
+
+@app.route('/categories/<int:category_id>')
+def catdetails(category_id):
+    header = database.catheader(category_id)
+    details =database.get_category_details(category_id)
+    return render_template ('catdetails.html', details=details, header=header)
--- database.py
+++ database.py
@@ -4,26 +4,21 @@ def supplier():
     conn=sqlite3.connect('Northwind_large.sqlite') 
     cur=conn.cursor()    
     cur.execute ('SELECT CompanyName, City, Country, Id FROM  Supplier')  
-    
     column_names = []
     for column in cur.description:
-        column_names.append(column[0])
-    
-    
+        column_names.append(column[0])  
     rows=cur.fetchall() 
     dicts=[]
-    
     for row in rows:
         d=dict(zip(column_names, row))
-        dicts.append(d)
-        
+        dicts.append(d)  
     conn.close() 
     return dicts 
 
 def products(supplier_id):
     conn=sqlite3.connect('Northwind_large.sqlite') 
     cur=conn.cursor()    
-    cur.execute ('SELECT ProductName, QuantityPerUnit, UnitPrice From Product Where SupplierId=?',(supplier_id,) )  
+    cur.execute ('SELECT CategoryId, ProductName, QuantityPerUnit, UnitPrice From Product Where SupplierId=?',(supplier_id,) )  
     column_names = []
     for column in cur.description:
         column_names.append(column[0])  
@@ -50,3 +45,53 @@ def supplier_name(supplier_id):
         dicts.append(d)   
     conn.close() 
     return str(*row)
+
+
+def category():
+    conn=sqlite3.connect('Northwind_large.sqlite') 
+    cur=conn.cursor()    
+    cur.execute (""" SELECT Category.Id,Category.CategoryName,Category.Description,  COUNT(Product.ProductName)  AS "Product Count" FROM Category
+                        INNER JOIN Product on Category.Id=Product.CategoryId
+                        GROUP BY Product.CategoryId""" )  
+    column_names = []
+    for column in cur.description:
+        column_names.append(column[0])  
+    rows=cur.fetchall() 
+    dicts=[]
+    for row in rows:
+        d=dict(zip(column_names, row))
+        dicts.append(d)   
+    conn.close() 
+    return dicts 
+    
+def get_category_details(category_id):
+    conn=sqlite3.connect('Northwind_large.sqlite') 
+    cur=conn.cursor()    
+    cur.execute ("""SELECT  Product.CategoryId, Product.ProductName,Supplier.CompanyName FROM Product
+                        INNER JOIN Supplier on Product.CategoryId= Supplier.Id
+                        WHERE CategoryId=?""", (category_id,) )  
+    column_names = []
+    for column in cur.description:
+        column_names.append(column[0])  
+    rows=cur.fetchall() 
+    dicts=[]
+    for row in rows:
+        d=dict(zip(column_names, row))
+        dicts.append(d)   
+    conn.close() 
+    return dicts 
+
+
+def catheader(category_id):
+    conn=sqlite3.connect('Northwind_large.sqlite') 
+    cur=conn.cursor()    
+    cur.execute ("""SELECT  CategoryName, Id    FROM Category
+                        WHERE Id=?""", (category_id,) )  
+   
+    rows=cur.fetchmany()     #!!!! plus precise which row
+    
+    for row in rows:
+        return row[0]        # precised here
+         
+    conn.close() 
+   
